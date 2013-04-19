2b860536b4f44b86df8da26448c7c694b1178979
marcs77 <kinsisi360@hotmail.com>
2013-04-19 14:53:49 +0200

    Creada clase de Reference con las propiedades generales del mod

---
+++ modprueba_common/com/marcs/modprueba/lib/Reference.java
@@ -0,0 +1,8 @@
+package com.marcs.modprueba.lib;
+
+public class Reference {
+    // Propiedades generales del mod
+    public static final String MOD_ID="ModPrueba";
+    public static final String MOD_NAME="Mod de Prueba";
+    public static final String VERSION="0.0.1";
+}
