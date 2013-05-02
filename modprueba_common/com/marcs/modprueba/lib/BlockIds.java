5559d69b9e7d8f24ed716d5c374b6fe8b8ccffc0
marcs77 <kinsisi360@hotmail.com>
2013-05-02 15:05:44 +0200

    Añadidas clases de referencias para IDs de bloques y para Strings.

---
+++ modprueba_common/com/marcs/modprueba/lib/BlockIds.java
@@ -0,0 +1,6 @@
+package com.marcs.modprueba.lib;
+
+public class BlockIds {
+    //Ids default de los bloques
+    public static final int TESTBLOCK = 500;
+}
---
+++ modprueba_common/com/marcs/modprueba/lib/Strings.java
@@ -0,0 +1,5 @@
+package com.marcs.modprueba.lib;
+
+public class Strings {
+    public static final String TESTBLOCK_NAME = "Bloque de prueba";
+}
