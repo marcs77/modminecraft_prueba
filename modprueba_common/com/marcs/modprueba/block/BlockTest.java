cfbb6a9a9943187cc64754d7a46e432fa4b4b2fc
marcs77 <kinsisi360@hotmail.com>
2013-05-04 23:43:25 +0200

    Añadida textura del bloque de prueba

--- modprueba_common/com/marcs/modprueba/block/BlockTest.java
+++ modprueba_common/com/marcs/modprueba/block/BlockTest.java
@@ -2,6 +2,7 @@ package com.marcs.modprueba.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 
 public class BlockTest extends Block{
@@ -9,5 +10,10 @@ public class BlockTest extends Block{
     public BlockTest(int ID){
         super(ID, Material.rock);
         this.setCreativeTab(CreativeTabs.tabBlock);
+        this.setHardness(3.0F);
+        this.setResistance(5.0F);
+    }
+    public void registerIcons (IconRegister reg){
+        this.blockIcon = reg.registerIcon("modprueba:"+this.getUnlocalizedName2());
     }
 }
