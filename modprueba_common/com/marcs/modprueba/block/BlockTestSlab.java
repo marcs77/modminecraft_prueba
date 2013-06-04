2eca91223d51cbdb47156aa77057ba09f0f67783
marcs77 <kinsisi360@hotmail.com>
2013-06-04 21:03:35 +0200

    Añadido sided textures

--- modprueba_common/com/marcs/modprueba/block/BlockTestSlab.java
+++ modprueba_common/com/marcs/modprueba/block/BlockTestSlab.java
@@ -1,8 +1,12 @@
 package com.marcs.modprueba.block;
 
+import com.marcs.modprueba.lib.Reference;
+
 import net.minecraft.block.BlockHalfSlab;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.util.Icon;
 
 
 public class BlockTestSlab extends BlockHalfSlab{
@@ -11,6 +15,23 @@ public class BlockTestSlab extends BlockHalfSlab{
         this.setCreativeTab(CreativeTabs.tabBlock);
         this.setHardness(3.0F);
         this.setResistance(5.0F);
+    }
+    private Icon icons[];
+    public void registerIcons(IconRegister reg){
+        icons = new Icon[2];
+        icons[0] = reg.registerIcon(Reference.MOD_ID+":"+ModBlocks.blockBismuthBrick.getUnlocalizedName2());
+        icons[1] = reg.registerIcon(Reference.MOD_ID+":"+ModBlocks.blockBismuthOre.getUnlocalizedName2());
+    }
+    public Icon getIcon(int par1, int par2){
+        switch(par1)
+        {
+               case 0:
+                     return icons[0];
+               case 1:
+                     return icons[0];
+               default:
+                     return icons[1];
+        }
     }
 
     @Override
