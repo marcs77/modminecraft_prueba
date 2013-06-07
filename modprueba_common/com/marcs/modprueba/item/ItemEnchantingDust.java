9459bcaae9a3b26d1c7982e1227c23101d963c56
marcs77 <kinsisi360@hotmail.com>
2013-06-07 14:58:19 +0200

    Añadido efecto estético de épico.

--- modprueba_common/com/marcs/modprueba/item/ItemEnchantingDust.java
+++ modprueba_common/com/marcs/modprueba/item/ItemEnchantingDust.java
@@ -6,7 +6,9 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.EnumRarity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 public class ItemEnchantingDust extends Item{
 
@@ -17,5 +19,13 @@ public class ItemEnchantingDust extends Item{
     @SideOnly(Side.CLIENT)
     public void registerIcons(IconRegister reg){
         this.itemIcon = reg.registerIcon(Reference.MOD_ID.toLowerCase() + ":" + this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf(".") + 1));
+    }
+    @SideOnly(Side.CLIENT)
+    public boolean hasEffect(ItemStack par1ItemStack){
+        return true;
+    }
+    @SideOnly(Side.CLIENT)
+    public EnumRarity getRarity(ItemStack par1ItemStack){
+        return EnumRarity.rare;
     }
 }
